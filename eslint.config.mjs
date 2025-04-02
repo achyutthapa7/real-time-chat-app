5dfec69da62b7615302421ba37257722ab8c90d1
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-04-02 17:03:16 +0545

    added registration login logout  middleware

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off", // Disable unescaped entity errors
+      "react-hooks/exhaustive-deps": "off", // Disable missing dependencies warning
+      "@typescript-eslint/no-explicit-any": "off", // Allow 'any' usage
+    },
+  }),
 ];
 
 export default eslintConfig;
